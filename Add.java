88b06c3a851e746bf1e02166cd378d2dd292eec5
sarwesh86 <sarweshkumar86@gmail.com>
2020-10-17 13:21:41 +0530

    02_prog= Additon of two number

---
+++ Add.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Add
+{
+public static void main(String[] args)
+{
+
+Scanner scan=new Scanner(System.in);
+System.out.println("please enter two number");
+int a=scan.nextInt();
+int b=scan.nextInt();
+
+System.out.println("Sum of two number is "+(a+b));
+
+}
+
+}

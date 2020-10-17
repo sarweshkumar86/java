b612230b76165854b1348c8c16dd4f8f2bf0f19c
sarwesh86 <sarweshkumar86@gmail.com>
2020-10-17 13:23:51 +0530

    03_prog= Display area of rectangle

---
+++ Rectangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Rectangle
+{
+public static void main(String[] args)
+{
+
+Scanner scan=new Scanner(System.in);
+System.out.println("please enter length and breath of a rectangle");
+float a=scan.nextFloat();
+float b=scan.nextFloat();
+
+System.out.println("Area of rectangle is "+(a*b));
+
+}
+
+}

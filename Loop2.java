92026e2f646d83d9e36d557ad04f2636945d6e26
sarweshkumar86 <71916533+sarweshkumar86@users.noreply.github.com>
2020-11-02 10:28:34 +0530

    09_prog=Display 500,300,100 using for loop

---
+++ Loop2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Loop2
+{
+public static void main(String[] args)
+{
+
+Scanner scan=new Scanner(System.in);
+System.out.println("Display 500,300,100 using for loop");
+
+
+
+for(int i=5;i>0;i=i-2) 
+{
+System.out.println(i*100);
+
+}
+
+
+}}

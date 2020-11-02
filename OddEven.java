9b3741cb6ad78eb948bbdcb9d8f3ae40431cea89
sarweshkumar86 <71916533+sarweshkumar86@users.noreply.github.com>
2020-11-02 10:15:21 +0530

    06_prog=To check the no is off or even

---
+++ OddEven.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class OddEven
+{
+public static void main(String[] args)
+{
+
+Scanner scan=new Scanner(System.in);
+System.out.println("please enter a number");
+int a=scan.nextInt();
+
+if(a%2==0)
+{
+System.out.println("The number is even");
+}
+
+else
+{
+System.out.println("The number is Odd");
+}
+
+
+}}

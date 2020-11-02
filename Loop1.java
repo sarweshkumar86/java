a28ce446c174ae0f21b009180cc1c10a8bae3e9f
sarweshkumar86 <71916533+sarweshkumar86@users.noreply.github.com>
2020-11-02 10:17:43 +0530

    08_prog=using for loop display 10 no

---
+++ Loop1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Loop1
+{
+public static void main(String[] args)
+{
+
+Scanner scan=new Scanner(System.in);
+System.out.println("Using foor loop display ten number");
+
+for(int i=0;i<10;i++) 
+{
+System.out.println(i);
+
+}
+
+
+}}

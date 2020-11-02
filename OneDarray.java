7485911ee809c281e26aa6858c1f8c71fcc108a3
sarweshkumar86 <71916533+sarweshkumar86@users.noreply.github.com>
2020-11-02 10:41:05 +0530

    17_prog=Display five number using array

---
+++ OneDarray.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class OneDarray
+{
+public static void main(String[] args)
+{
+
+Scanner scan=new Scanner(System.in);
+System.out.println("Display five number array");
+int a[]=new int[]{10,20,30,40,50};
+
+for(int i=0;i<5;i++)
+{
+System.out.println(a[i]);
+
+}
+
+
+
+}}

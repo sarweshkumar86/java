ba46948177aa3b8a170456d30b65aaf1110cc0b7
sarweshkumar86 <71916533+sarweshkumar86@users.noreply.github.com>
2020-11-02 10:34:42 +0530

    12_prog= Display 10 number  using do while loop

---
+++ WhileLoop3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class WhileLoop3
+{
+public static void main(String[] args)
+{
+
+Scanner scan=new Scanner(System.in);
+System.out.println("Display 10 number  using do while loop");
+int i=1;
+do
+{
+System.out.println(i);
+i++;
+}
+while(i<10);
+
+}}

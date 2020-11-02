6652a38dbe89b405fffc085f72b3e47dd0040ee4
sarweshkumar86 <71916533+sarweshkumar86@users.noreply.github.com>
2020-11-02 10:10:45 +0530

    04_prog=Display greater no between two no using if else

---
+++ EfElse.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class EfElse
+{
+public static void main(String[] args)
+{
+
+Scanner scan=new Scanner(System.in);
+System.out.println("please enter two number");
+int a=scan.nextInt();
+int b=scan.nextInt();
+
+if(a>b)
+{
+System.out.println("Greater number is "+ a);
+}
+
+else
+{
+System.out.println("Greaer number is "+b);
+}
+
+
+}}

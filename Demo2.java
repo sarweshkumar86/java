be017c49e6a91f9144aa0c6199ce313c86a90d93
sarweshkumar86 <71916533+sarweshkumar86@users.noreply.github.com>
2020-11-09 11:35:21 +0530

    prog_wrapper= "warpper class"

---
+++ Demo2.java
@@ -0,0 +1,13 @@
+
+class Demo2
+{
+
+public static void main(String a[])
+{
+
+int s1=Integer.parseInt(a[0]);
+int s2=Integer.parseInt(a[1]);
+
+System.out.println(" first name is  " +(s1+s2));
+}
+}

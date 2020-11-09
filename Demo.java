27e09ff9f5e1d7479997be3651ee5877fa29603b
sarweshkumar86 <71916533+sarweshkumar86@users.noreply.github.com>
2020-11-09 14:06:46 +0530

    prog=interface= "Example of interface"

--- Demo.java
+++ Demo.java
@@ -1,34 +1,25 @@
-class Test
+interface Testt
 {
-static  int a=10;
-static int b=10;
-static void add()
-{
-System.out.println("sum of two number is "+(a+b));
+public void name();
 }
 
-static void mult()
-{
-int aa=20;
-int bb=20;
-System.out.println("Multiplication of two number is "+(aa*bb));
-}
 
+class Test implements Testt
+{
+public void name()
+{
+System.out.println(" hello I am sarwesh kumar");
+} 
 }
 
 class Demo
 {
-//static int a=10;
-//static int b=10;
-
-
 public static void main(String[] args)
 {
-System.out.println("helllo this is main class");
-//System.out.println(a+b);
-//System.out.println(Test.a+Test.b);
-//Test.add();
-Test.mult();
+Test t=new Test();
+t.name();
 
 }
+
+
 }

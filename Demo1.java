409f6b148524d5bb0e7b4406d2d125397ce96505
sarweshkumar86 <71916533+sarweshkumar86@users.noreply.github.com>
2020-11-09 11:07:32 +0530

    prog="command line argument"

---
+++ Demo1.java
@@ -0,0 +1,15 @@
+
+class Demo1
+{
+
+public static void main(String a[])
+{
+
+String s1=a[0];
+String s2=a[1];
+String s3=a[2];
+System.out.println(" first name is  " +s1);
+System.out.println(" Second  name is  " +s2);
+System.out.println(" Third  name is  " +s3);
+}
+}
